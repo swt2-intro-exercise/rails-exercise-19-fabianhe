92e5fb9d4a5ab121bbc0d0f99db89a47710221f9
fabianhe <fabian.heseding@web.de>
2019-11-05 14:56:52 +0100

    Made failing test for #3 pass by implementing author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe "Create new author", type: :model do
  
  it "should create a new author with the first name Alan, the last name Turing, with the homepage http://wikipedia.org/Alan_Turing, and with the name Alan Turing" do
-    author = Author.new(first_name="Alan", last_name="Turing", homepage="http://wikipedia.org/Alan_Turing")
+  author = Author.new(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")
   expect(author.first_name).to eq("Alan")
   expect(author.last_name).to eq("Turing")
-  expect(auhtor.homepage).to eq("http://wikipedia.org/Alan_Turing")
+  expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
   expect(author.name).to eq("Alan Turing")
   end
 end
