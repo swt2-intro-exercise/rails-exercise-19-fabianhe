513dd30e80bcc4f41cc6fe06f6f53a111a664ba6
fabianhe <fabian.heseding@web.de>
2019-11-07 10:50:59 +0100

    Failing test for #7

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,9 @@ describe "Create new author", type: :model do
   expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
   expect(author.name).to eq("Alan Turing")
   end
+
+  it "should not create an author without a last name" do
+    author = Author.new(first_name:"Alan", last_name:"", homepage:"http://wikipedia.org/Alan_Turing")
+    expect(author).to_not be_valid
+    end
 end
