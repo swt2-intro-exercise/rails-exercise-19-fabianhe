930594b01dca2e63d1c2ce48707f0d1cc077d89e
fabianhe <fabian.heseding@web.de>
2019-11-07 11:05:25 +0100

    Failing test for #8

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe "New author page", type: :feature do
  
@@ -10,17 +10,26 @@ describe "New author page", type: :feature do
   visit new_author_path
   
   # these are the standard names given to inputs by the form builder
-  expect(page).to have_field('author[first_name]')
-  expect(page).to have_field('author[last_name]')
-  expect(page).to have_field('author[homepage]')
+  expect(page).to have_field("author[first_name]")
+  expect(page).to have_field("author[last_name]")
+  expect(page).to have_field("author[homepage]")
 end
 it "should save the author" do
   visit new_author_path
   expect{
-    page.fill_in 'author[first_name]', with: 'Alan'
-    page.fill_in 'author[last_name]', with: 'Turing'
-    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
-    find('input[type="submit"]').click
+    page.fill_in "author[first_name]", with: "Alan"
+    page.fill_in "author[last_name]", with: "Turing"
+    page.fill_in "author[homepage]", with: "http://wikipedia.org/Alan_Turing"
+    find("input[type='submit']").click
   }.to change(Author, :count).by(1)
+end
+
+it "should show the user a validation error upon omitting the last name attribute" do
+  visit new_author_path
+  page.fill_in "author[first_name]", with: "Alan"
+  page.fill_in "author[last_name]", with: ""
+  page.fill_in "author[homepage]", with: "http://wikipedia.org/Alan_Turing"
+  find("input[type='submit']").click
+  expect(page).to have_text("error")
 end
 end
