151196777ee4fef0492ae99bde0be3dd214b451c
fabianhe <fabian.heseding@web.de>
2019-11-07 11:28:02 +0100

    Failing test for #9

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "Edit author page", type: :feature do
+    alan = FactoryBot.create :author
+
+    it "should render without error" do
+        visit edit_author_path(alan)
+    end
+
+    it "should write changes made to the authors in an edit form to the database" do
+        visit edit_author_path(alan)
+        page.fill_in "author[homepage]", with: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        find("input[type='submit']").click
+        alan.reload
+        expect(alan.homepage).to eq("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    end
+end
