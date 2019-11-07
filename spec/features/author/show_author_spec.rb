5dd988e73d9b8c92ad2ca745ae36b077b425ffcb
fabianhe <fabian.heseding@web.de>
2019-11-07 10:10:30 +0100

    Failing test for #5

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Show author page", type: :feature do
+ 
+ it "should render withour error" do
+   visit author_path(@alan)
+   expect(page).to have_text("Alan")
+   expect(page).to have_text("Turing")
+   expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+ end
+
+end
